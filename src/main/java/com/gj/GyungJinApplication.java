a8ec04ea9c5cce2fed35069c6c21121a58cef530
developerJY <sky14786@gmail.com>
2020-02-07 00:27:00 +0900

    Project Settings

---
+++ src/main/java/com/gj/GyungJinApplication.java
@@ -0,0 +1,13 @@
+package com.gj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyungJinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyungJinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GyungJin/GyungJinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GyungJin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyungJinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
