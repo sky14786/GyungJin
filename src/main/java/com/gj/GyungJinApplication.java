43764a05c534a2962543fe540edf3cda6f673df0
developerJY <sky14786@gmail.com>
2020-02-13 16:01:20 +0900

    Added Login page

--- src/main/java/com/gj/GyungJinApplication.java
+++ src/main/java/com/gj/GyungJinApplication.java
@@ -2,6 +2,11 @@ package com.gj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class GyungJinApplication {
@@ -9,5 +14,5 @@ public class GyungJinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GyungJinApplication.class, args);
 	}
-
+	
 }
