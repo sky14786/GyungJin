e261d10484ef1e2ee175101f185f1aea8e4483fe
developerJY <sky14786@gmail.com>
2020-03-30 17:32:14 +0900

    Renamed Path

--- src/main/java/com/gj/HomeController.java
+++ src/main/java/com/gj/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping("/erp/")
+	@GetMapping("/erp")
 	public String connectionHome() {
 		return "erpindex";
 	}
