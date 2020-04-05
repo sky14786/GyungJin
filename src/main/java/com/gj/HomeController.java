e2ea1d84c765b1c03c11a00da38659afc0f84a27
developerJY <sky14786@gmail.com>
2020-04-05 18:49:47 +0900

    Added Html File

--- src/main/java/com/gj/HomeController.java
+++ src/main/java/com/gj/HomeController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	@GetMapping("/erp")
-	public String connectionHome() {
+	public String connectionErpHome() {
 		return "erpindex";
+	}
+	
+	@GetMapping("/")
+	public String connectionHome() {
+		return "index";
 	}
 }
