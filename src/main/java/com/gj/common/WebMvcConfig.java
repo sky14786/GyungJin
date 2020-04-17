6265aa97423b792f1dd39a03a8c3ef716d2b71ff
developerJY <sky14786@gmail.com>
2020-04-17 14:05:50 +0900

    Updated Estimate

--- src/main/java/com/gj/common/WebMvcConfig.java
+++ src/main/java/com/gj/common/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		final List<String> URL_PATTERNS = Arrays.asList("/", "/view/**");
-		registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/**").excludePathPatterns(URL_PATTERNS);
+		registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/api/**").excludePathPatterns(URL_PATTERNS);
 		registry.addInterceptor(new JwtInterceptor()).addPathPatterns("/api/**");
 	}
 
